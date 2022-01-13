96c4f51da2725488514f136e3000969207f22bf2
swiftsuretech <dave@davewhitehouse.co.uk>
2022-01-13 13:46:41 +0000

    Updated bastion prep

--- variables.tf
+++ variables.tf
@@ -5,8 +5,13 @@ variable "aws_profile" {
   sensitive   = true
 }
 
-variable "buckt_name" {
+variable "bucket_name" {
   description = "Bucket with binaries"
-  type = string
-  default = "btsec-pov"
+  type        = string
+  default     = "btsec-pov"
+}
+
+variable "bundle_name" {
+  description = "The air gapped bundle tarball"
+  default     = "konvoy_image_bundle_v2.1.1_linux_amd64.tar.gz"
 }
