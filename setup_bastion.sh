ed81227bb36fb95d3a2994870bef536e1bef2ebe
swiftsuretech <dave@davewhitehouse.co.uk>
2022-01-13 13:48:20 +0000

    Updated bastion prep

--- setup_bastion.sh
+++ setup_bastion.sh
@@ -13,3 +13,10 @@ sudo usermod -aG docker centos
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Disable firewalld
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+# Disable swap
+swapoff -a
