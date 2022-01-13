697ffed58d5034a834a6b5a810de67656e7d577f
swiftsuretech <dave@davewhitehouse.co.uk>
2022-01-13 17:57:33 +0000

    Remove S3

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket_object" "bundle" {
-  bucket = var.bucket_name
-  key    = var.bundle_name
-}
+#  resource "aws_s3_bucket_object" "bundle" {
+#   bucket = var.bucket_name
+#  key    = var.bundle_name
+# }
