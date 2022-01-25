7661582dec47d0e124139aa000c106790312c8a1
swiftsuretech <dave@davewhitehouse.co.uk>
2022-01-25 11:23:53 +0000

    opened p5000 for registry

--- infrastructure/securitygroups.tf
+++ infrastructure/securitygroups.tf
@@ -8,6 +8,12 @@ resource "aws_security_group" "btsec-pov-bastion-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
