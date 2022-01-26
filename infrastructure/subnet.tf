1eda541d8ab0d6816131dfc27394bafe26b09053
swiftsuretech <dave@davewhitehouse.co.uk>
2022-01-26 19:51:50 +0000

    added 2ary subnet

--- infrastructure/subnet.tf
+++ infrastructure/subnet.tf
@@ -1,12 +1,28 @@
 resource "aws_subnet" "btsec-pov-subnet" {
   vpc_id                  = aws_vpc.btsec-pov-vpc.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.0.0/16"
   map_public_ip_on_launch = true
   availability_zone       = "us-west-2b"
   tags = (merge(
     var.tags,
     tomap({
       "Name" : "${var.cluster_name}-subnet",
+      "kubernetes.io/cluster/${var.cluster_name}" : "owned",
+      "kubernetes.io/cluster" : "${var.cluster_name}"
+      }
+    )
+  ))
+}
+
+resource "aws_subnet" "btsec-pov-subnet-2" {
+  vpc_id                  = aws_vpc.btsec-pov-vpc.id
+  cidr_block              = "10.10.0.0/16"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+  tags = (merge(
+    var.tags,
+    tomap({
+      "Name" : "${var.cluster_name}-subnet-2",
       "kubernetes.io/cluster/${var.cluster_name}" : "owned",
       "kubernetes.io/cluster" : "${var.cluster_name}"
       }
