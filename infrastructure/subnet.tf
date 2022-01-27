21295af7c7b2c018ed963cd96f723c0d1aa12893
swiftsuretech <dave@davewhitehouse.co.uk>
2022-01-27 13:40:06 +0000

    updated subnet allocs

--- infrastructure/subnet.tf
+++ infrastructure/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "btsec-pov-subnet" {
   vpc_id                  = aws_vpc.btsec-pov-vpc.id
-  cidr_block              = "10.0.0.0/16"
+  cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-west-2b"
   tags = (merge(
@@ -16,7 +16,7 @@ resource "aws_subnet" "btsec-pov-subnet" {
 
 resource "aws_subnet" "btsec-pov-subnet-2" {
   vpc_id                  = aws_vpc.btsec-pov-vpc.id
-  cidr_block              = "10.10.0.0/16"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-west-2a"
   tags = (merge(
