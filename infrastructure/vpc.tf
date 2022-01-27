66f54b85c9cdc8046f99e3594d49cce698788fa2
swiftsuretech <dave@davewhitehouse.co.uk>
2022-01-27 13:40:26 +0000

    bundled single CIDR block

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,13 +1,8 @@
 resource "aws_vpc" "btsec-pov-vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/23"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     "Name" = "btsec-vpc"
   }
-}
-
-resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
-  vpc_id     = aws_vpc.btsec-pov-vpc.id
-  cidr_block = "10.10.0.0/16"
 }
