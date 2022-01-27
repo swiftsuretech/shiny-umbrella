7813667ac0cc77acc3345f575692d4a733e06e20
swiftsuretech <dave@davewhitehouse.co.uk>
2022-01-27 10:37:23 +0000

    refactored internal sg

--- infrastructure/securitygroups.tf
+++ infrastructure/securitygroups.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "btsec-pov-bastion-sg" {
 }
 
 
-resource "aws_security_group" "btset-pov-private-sg" {
+resource "aws_security_group" "btsec-pov-internal-sg" {
   description = "Allow all communication between instances"
   vpc_id      = aws_vpc.btsec-pov-vpc.id
   name        = "BTSEC POV nodes security group"
@@ -52,4 +52,71 @@ resource "aws_security_group" "btset-pov-private-sg" {
       }
     )
   ))
+}
+
+
+resource "aws_security_group" "btsec-pov-lb" {
+  description = "Load Banlancer Security Group"
+  vpc_id      = aws_vpc.btsec-pov-vpc.id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    self      = true
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    self      = true
+  }
+
+  ingress {
+    from_port = 9000
+    to_port   = 9000
+    protocol  = "tcp"
+    self      = true
+  }
+
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  tags = (merge(
+    var.tags,
+    tomap({
+      "Name" : "${var.cluster_name}-sg-elb",
+      "kubernetes.io/cluster/${var.cluster_name}" : "owned",
+      "kubernetes.io/cluster" : "${var.cluster_name}"
+      }
+    )
+  ))
+}
+
+resource "aws_security_group" "btsec-pov-control-plane-sg" {
+  description = "Allow traffic to konvoy control plane"
+  vpc_id      = aws_vpc.btsec-pov-vpc.id
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = (merge(
+    var.tags,
+    tomap({
+      "Name" : "${var.cluster_name}-cp-elb",
+      "kubernetes.io/cluster/${var.cluster_name}" : "owned",
+      "kubernetes.io/cluster" : "${var.cluster_name}"
+      }
+    )
+  ))
+
 }
